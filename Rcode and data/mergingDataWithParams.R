f3e957923b2f927998fdf0fca9712edc5a3e3509
Rijk <r.a.mercuur@tudelft.nl>
2019-08-13 14:55:06 +0200

    More Data

--- Rcode and data/mergingDataWithParams.R
+++ Rcode and data/mergingDataWithParams.R
@@ -1,9 +1,3 @@
-dat <- read.csv("./IndividualAll.2019.Aug.13.14_17_22.txt")
-params <- read.csv("./IndividualAll.2019.Aug.13.14_17_22.batch_param_map.txt")
-datParams <- merge(lastAction,valueDifference, by = c("run","tick","ID"))
-
-
-complete <- read.csv("./Data For Luciano/IndividualResponseAndValueDifference.2019.Jul.03.10_24_48.txt")
-
-
-dat2 <- read.csv("./Data For Luciano/IndividualAll.2019.Jul.24.17_27_42.txt")
+dat <- read.csv("./IndividualAll.2019.Aug.13.14_32_15.txt")
+params <- read.csv("./IndividualAll.2019.Aug.13.14_32_15.batch_param_map.txt")
+datParams <- merge(dat,params, by = c("run"))
